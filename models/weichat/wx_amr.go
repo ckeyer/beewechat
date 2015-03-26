7f2b47cc94c58d2a5ec3f19f5267857c67f9139f
ckeyer <wangcj1214@gmail.com>
2015-03-26 20:06:12 +0800

    add models/weichat

---
+++ models/weichat/wx_amr.go
@@ -0,0 +1,12 @@
+package weichat
+
+type WX_AMR struct {
+	ToUserName   string `xml : "ToUserName"`   //开发者微信号
+	FromUserName string `xml : "FromUserName"` //发送方帐号（一个OpenID）
+	CreateTime   string `xml : "CreateTime"`   //消息创建时间 （整型）
+	MsgType      string `xml : "MsgType"`      //语音为voice
+	MediaID      string `xml : "MediaID"`      //语音消息媒体id，可以调用多媒体文件下载接口拉取该媒体
+	Format       string `xml : "Format"`       //语音格式：amr
+	Recognition  string `xml : "Recognition"`  //语音识别结果，UTF8编码
+	MsgID        int64  `xml : "MsgID"`        //消息id，64位整型
+}
