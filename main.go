decb15d097a1306c1fd4c7f9bc1428a37b7940d5
ckeyer <wangcj1214@gmail.com>
2015-04-14 19:14:01 +0800

    up

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
-	"github.com/ckeyer/beeweichat/models"
+	"github.com/ckeyer/beewechat/models"
 	_ "lab204/routers"
 )
 
